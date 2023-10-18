5de58512023c0c2bb0d6c99eefac601f33c1f8a1
zhangzhenping <zhangzhenping23@gmail.com>
2023-10-18 13:04:33 +0000

    feat: lab2 complete Implementing the TCP receiver

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -11,9 +11,29 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    if (seg.header().syn) {
+        _start_flag = true;
+        this->_isn = seg.header().seqno;
+    }
+    if (!_start_flag)
+        return;
+    uint64_t checkpoint = _reassembler.stream_out().bytes_written() + 1;
+    uint64_t substring_stream_index = unwrap(seg.header().syn ? seg.header().seqno + 1 : seg.header().seqno, this->_isn, checkpoint) - 1;
+    bool eof = seg.header().fin;
+    this->_reassembler.push_substring(string(seg.payload().str()), substring_stream_index, eof);
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const {
+    if (_start_flag) {
+        auto temp1 = wrap(_reassembler.stream_out().bytes_written() + 1, this->_isn);
+        uint32_t eof_1 = (_reassembler.stream_out().input_ended() ? 1 : 0);
+        auto result = temp1 + eof_1;
+        return result;
+    } else {
+        return nullopt;
+    }
+}
 
-size_t TCPReceiver::window_size() const { return {}; }
+size_t TCPReceiver::window_size() const {
+    return _reassembler.stream_out().remaining_capacity();
+}
